d86e7173030101fd2985159ae10ac43bd7281483
LuisAristi <santiaristi2015@gmail.com>
2024-06-09 06:27:57 -0500

    algunas aclaraciones importantes

--- includes.h
+++ includes.h
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+#include <locale.h>
 
-
-using namespace std;
+//porfavor no declarar funciones aqui, cosas malas pasan
+//esta carpeta solo acepta structs 
+//acostumbrense a escribir std:: antes de cada cosa que lo requiera
+//no usaremos using namespace std
