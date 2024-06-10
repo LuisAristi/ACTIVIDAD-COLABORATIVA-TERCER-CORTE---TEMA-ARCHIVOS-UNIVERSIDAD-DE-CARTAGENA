5784c770ffbf2ea450f7d9d2389bd99c0ba5c044
LuisAristi <santiaristi2015@gmail.com>
2024-06-10 15:25:17 -0500

    structs basicos de gestion de viajes

--- includes.h
+++ includes.h
@@ -4,3 +4,29 @@
 #include <locale.h>
 
 //declarar strutcs que se usaran en todo el proyecto aqui
+
+//struct de rutas
+struct tiempo{
+    int hora;
+    int minuto;
+};
+
+struct semana{
+    bool lunes;
+    bool martes;
+    bool miercoles;
+    bool jueves; 
+    bool viernes;
+    bool sabado;
+    bool domingo;
+};
+
+struct servicios{
+    semana dias;
+    tiempo salida;
+    tiempo llegada;
+    char lugarDeOrigen[50];
+    char lugarDestino[50];
+
+
+};
