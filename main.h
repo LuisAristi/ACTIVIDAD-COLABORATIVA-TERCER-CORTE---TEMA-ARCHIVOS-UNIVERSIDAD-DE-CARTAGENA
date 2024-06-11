68965f7368125d34d0290a3472b2d61cd2717973
LuisAristi <santiaristi2015@gmail.com>
2024-06-11 15:21:18 -0500

    dejando el main sin funciones, pronto se optimizara ese switch case

---
+++ main.h
@@ -0,0 +1,34 @@
+#include "premiosPromociones.h"
+
+//no es una pesadilla y altamente ineficiente hacer lo que 
+    //estamos haciendo? si lo es, pero se ve bonito 
+        //-- frases que dice el ingeniero civil 
+            //antes que se caiga el puente
+
+void mostrarMenuPrincipal() {
+    std::cout << "\nMenú Principal\n";
+    std::cout << "Gestión Viaje\n";
+    std::cout << "---- 1. Registrar Rutas Turísticas\n";
+    std::cout << "---- 2. Registrar Viaje\n";
+    std::cout << "------- 3. Imprimir Recorrido del Viaje (Por Cliente)\n";
+    std::cout << "------- 4. Imprimir Recorrido del Viaje (Folleto)\n";
+    std::cout << "Gestión De Automóviles\n";
+    std::cout << "---- 5. Registrar Vehículo\n";
+    std::cout << "---- 6. Registrar Conductor\n";
+    std::cout << "---- 7. Asignación de Conductores\n";
+    std::cout << "---- 8. Registrar Revisión de Vehículo\n";
+    std::cout << "Consultar Rutas\n";
+    std::cout << "---- 9.  Según Fecha Indicada\n";
+    std::cout << "---- 10. Según Conductor\n";
+    std::cout << "---- 11. Según Vehículo\n";
+    std::cout << "---- 12. Promedio de Viajeros por Ruta\n";
+    std::cout << "Consultas Vehículo\n";
+    std::cout << "---- 13. Por Vehículo\n";
+    std::cout << "---- 14. Listado de KM por Vehículo\n";
+    std::cout << "---- 15. Listado de KM por Conductor\n";
+    std::cout << "Premios y Promociones\n";
+    std::cout << "---- 16. Listado de Mejores Clientes\n";
+    std::cout << "---- 17. Ingresar Cliente para Sorteo\n";
+    std::cout << "---- 18. Generar Sorteo\n";
+    std::cout << "19. Salir\n";
+}
