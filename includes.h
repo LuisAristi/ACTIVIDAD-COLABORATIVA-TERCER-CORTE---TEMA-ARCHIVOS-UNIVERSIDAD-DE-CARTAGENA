f19bce5054e8f8f8aed787c3e4371de948c83edc
LuisAristi <santiaristi2015@gmail.com>
2024-06-11 15:08:12 -0500

    funcion para validar numeros que maneja errores

--- includes.h
+++ includes.h
@@ -3,13 +3,25 @@
 #include <stdio.h>
 #include <locale.h>
 #include <fstream>
+#include <limits>
+
+//prototipos porque el orden importa
+long ingresarNumero(long numero);
+void clear();
+
 
 //declarar structs que se usaran en todo el proyecto aqui
-
 //struct de rutas || ESTO VA PA LARGO DE 9 a 
 struct tiempo{
     int hora;
     int minuto;
+};
+
+struct lugares{
+char nombreLugar[100]; 
+tiempo previstoDeLlegada; // quiero mostrar toda la informacion sobre los sitios de interes 
+tiempo previstoDeParada;
+char actividad[100];
 };
 
 //probando si esto funciona, creo que sera mejor usar un vector pero quiero ver si es viable asi
@@ -51,12 +63,25 @@ struct cliente{
 	long telefonoCliente;
 };
 
-struct lugares{
-char nombreLugar[100]; 
-tiempo previstoDeLlegada; // quiero mostrar toda la informacion sobre los sitios de interes 
-tiempo previstoDeParada;
-char actividad[100];
-};
 
-// validaciones
+// funciones de validaciones
+long ingresarNumero(long numero){ //validar que es un numero
+    malapraxi:
+    std::cout<<"recuerde ingresar un numero\n";
+    std::cin>>numero;
+    if(std::cin.fail()){
+        std::cin.clear();
+        clear();
+        std::cout<<"recuerde ingresar un numero valido, ingrese cualquier tecla para continuar\n";
+        char c = getchar();
+        clear();
+        goto malapraxi; // oficial en mi defensa, mi vida corria peligro 
+    }
+    return numero;
+}
+
+// funciones utilitarias
+void clear(){
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
